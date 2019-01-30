9f6f85f5f995a39f1d7ef62b7a3c4004081118d7
Elisiano Petrini <elisiano@gmail.com>
2019-01-30 14:53:32 -0500

    remapped backlit key to emoji (MacOS)

--- keyboards/preonic/keymaps/elisiano/keymap.c
+++ keyboards/preonic/keymaps/elisiano/keymap.c
@@ -34,7 +34,8 @@ enum preonic_keycodes {
   LOWER,
   RAISE,
   BACKLIT,
-  MOUSE
+  MOUSE,
+  EMOJI
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -52,12 +53,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
+// replaced BACKLIT with EMOJI since I have no LEDs
 [_QWERTY] = LAYOUT_preonic_grid( \
   KC_GRV,           KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
   KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,  \
   LCTL_T(KC_ESC),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  \
-  BACKLIT,          KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+  EMOJI,            KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
 
 /* Colemak
@@ -181,6 +183,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case QWERTY:
           if (record->event.pressed) {
             set_single_persistent_default_layer(_QWERTY);
+          }
+          return false;
+          break;
+        case EMOJI:
+          if (record->event.pressed) {
+            SEND_STRING(SS_DOWN(X_LCTRL)SS_DOWN(X_LGUI)SS_TAP(X_SPACE)SS_UP(X_LGUI)SS_UP(X_LCTRL));
           }
           return false;
           break;
