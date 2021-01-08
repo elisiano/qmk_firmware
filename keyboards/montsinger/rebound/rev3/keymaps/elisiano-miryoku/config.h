95822af4c17ecadb793ac3842c9c3ba7a557beab
Elisiano Petrini <elisiano@gmail.com>
2021-01-08 09:22:46 -0500

    [keymap] montsinger/rebound/rev3: tuned elisiano-miryoku params

--- keyboards/montsinger/rebound/rev3/keymaps/elisiano-miryoku/config.h
+++ keyboards/montsinger/rebound/rev3/keymaps/elisiano-miryoku/config.h
@@ -4,13 +4,13 @@
 #pragma once
 
 // default but used in macros
-#define TAPPING_TERM 200
+#define TAPPING_TERM 275
 
 // Prevent normal rollover on alphas from accidentally triggering mods.
 #define IGNORE_MOD_TAP_INTERRUPT
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
-#define TAPPING_FORCE_HOLD
+//  #define TAPPING_FORCE_HOLD  // i like repeat more
 
 // Auto Shift and Retro Shift (Auto Shift for Tap Hold).
 #define AUTO_SHIFT_TIMEOUT TAPPING_TERM
--- keyboards/montsinger/rebound/rev3/keymaps/elisiano-miryoku/rules.mk
+++ keyboards/montsinger/rebound/rev3/keymaps/elisiano-miryoku/rules.mk
@@ -1,3 +1,3 @@
 MOUSEKEY_ENABLE = yes # Mouse keys
 EXTRAKEY_ENABLE = yes # Audio control and System control
-AUTO_SHIFT_ENABLE = yes # Auto Shift
+# AUTO_SHIFT_ENABLE = yes # Auto Shift # does not make much sense given the home row has modifiers
